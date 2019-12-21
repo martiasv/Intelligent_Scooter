d920709717196625fa4b942168b6de859c24a693
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2019-12-21 17:03:19 +0100

    playing around with python

---
+++ Hello_machine_learning.py
@@ -0,0 +1,11 @@
+#Hello machine learning
+
+vector_1 = ['Hei','paa','deg','din','gamle','sjokolade']
+vecto_2 = ['Hade','paa','badet','din','gamle','sjokolade']
+
+for i in vector_1:
+    for j in vecto_2:
+        string = i + j
+        print(string)
+    
+
