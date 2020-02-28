3b2e8c7e752d9a193e671d735b48ad153f2119f6
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2020-02-28 12:53:40 +0100

    ready to merge

--- Traffic_light_detection/Faster_RCNN.py
+++ Traffic_light_detection/Faster_RCNN.py
@@ -1,5 +1,15 @@
 #importing required libraries
 import pandas as pd  
 import matplotlib.pyplot as plt 
-%matplotlib inline 
+#matplotlib inline 
 from matplotlib import patches
+import cv2
+
+#read the csv file using read_csv function of pandas
+train = pd.read_csv('../Datasets/BCCD_Dataset-master/test.csv')
+train.head()
+
+#reading single image using imread function to matplotlib
+image = plt.imread('../Datasets/BCCD_Dataset-master/BCCD/JPEGImages/BloodImage_00000.jpg')
+plt.show(image)
+cv2.waitKey(0)
