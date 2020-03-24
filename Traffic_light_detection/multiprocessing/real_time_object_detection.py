25cdf6cacad293e89d271da29ea9542c652cca8f
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2020-03-24 13:33:57 +0100

    Now with threading

--- Traffic_light_detection/multiprocessing/real_time_object_detection.py
+++ Traffic_light_detection/multiprocessing/real_time_object_detection.py
@@ -2,6 +2,12 @@
 # python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 # python3 real_time_object_detection.py --prototxt graph.pbtxt --model frozen_inference_graph.pb
 # import the necessary packages
+# threading: python3 real_time_object_detection.py --prototxt graph.pbtxt --model frozen_inference_graph.pb --num-frames 2600 --display 1
+
+# FPS before threading: 20.3
+# FPS after threading: 19.7
+from __future__ import print_function
+from readThread import readFromThread
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
@@ -19,6 +25,10 @@ ap.add_argument("-m", "--model", required=True,
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
 args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
@@ -32,47 +42,51 @@ net = cv2.dnn.readNetFromTensorflow(args["model"], args["prototxt"])
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = cv2.VideoCapture('video.mp4')
-time.sleep(2.0)
+#print("[INFO] starting video stream...")
+#vs = cv2.VideoCapture('video.mp4')
+#time.sleep(2.0)
+#fps = FPS().start()
+
+# created a *threaded* video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+
+#Where to look
+SRC = 'video.mp4'
+threading =1
+
+if threading:
+	print("[INFO] sampling THREADED frames from webcam...")
+	vs = readFromThread(src=SRC).start()
+else:
+	# initialize the video stream, allow the cammera sensor to warmup
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=SRC).start()
+	time.sleep(2.0)
+
 fps = FPS().start()
 
+
 # loop over the frames from the video stream
-while True:
+while fps._numFrames < args["num_frames"]:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	start_importim = datetime.datetime.now()
-	ret, frame = vs.read()
-	end_importim = datetime.datetime.now() - start_importim
-	print("[TIMESTAMP]: Time elapsed importing image: ")
-	print(end_importim.microseconds)
-	if not ret:
-		print("[INFO] end of video")
-		break
-	start_imresize = datetime.datetime.now()
-	frame = imutils.resize(frame, width=400)
-	end_imresize = datetime.datetime.now()-start_imresize
-	print("[TIMESTAMP]: Time elapsed resizing image: ")
-	print(end_imresize.microseconds)
-
-	start_blobify = datetime.datetime.now()
+	frame = vs.read()
+	#if not ret:
+	#	print("[INFO] end of video")
+	#	break
 	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
+	try:
+		(h, w) = frame.shape[:2]
+	except:
+		break
 	blob = cv2.dnn.blobFromImage(frame,size=(300, 300), swapRB=True, crop=False)
-	end_blobify = datetime.datetime.now() -start_blobify
-	print("[TIMESTAMP]: Time elapsed blobbifying image: ")
-	print(end_blobify.microseconds)
 	# pass the blob through the network and obtain the detections and
 	# predictions
 
-	start_netpass = datetime.datetime.now()
 	net.setInput(blob)
 	detections = net.forward()
-	end_netpass = datetime.datetime.now() - start_netpass
-	print("[TIMESTAMP]: Time elapsed passing image through network: ")
-	print(end_netpass.microseconds)
 	
-	start_prettydrawing = datetime.datetime.now()
 	# loop over the detections
 	for i in np.arange(0, detections.shape[2]):
 		# extract the confidence (i.e., probability) associated with
@@ -101,9 +115,6 @@ while True:
 	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
-	end_prettydrawing = datetime.datetime.now() - start_prettydrawing
-	print("[TIMESTAMP]: Time elapsed doing pretty drawing: ")
-	print(end_prettydrawing.microseconds)
 
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
@@ -111,9 +122,11 @@ while True:
 
 	# update the FPS counter
 	fps.update()
+	
 
 # stop the timer and display FPS information
 fps.stop()
+vs.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
